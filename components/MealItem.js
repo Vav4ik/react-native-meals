3df0d2816f960661901d065a05e09a0f70a21b54
Valdis Indricans <vav4ik@pop-os.localdomain>
2023-08-28 16:31:59 +0100

    Lesson 101

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,11 +1,81 @@
-import { Text, View } from "react-native";
+import {
+  Image,
+  Platform,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 const MealItem = ({ meal }) => {
+  const { affordability, complexity, duration, imageUrl, title } = meal;
+
   return (
-    <View>
-      <Text>{meal.title}</Text>
+    <View style={styles.mealItem}>
+      <Pressable
+        android_ripple={{ color: "#ccc" }}
+        style={({ pressed }) => pressed && styles.buttonPressed}
+      >
+        <View>
+          <View style={styles.innerContainer}>
+            <Image source={{ uri: imageUrl }} style={styles.image} />
+            <Text style={styles.title}>{title}</Text>
+          </View>
+          <View style={styles.details}>
+            <Text style={styles.detailItem}>{duration} min</Text>
+            <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
+            <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
+          </View>
+        </View>
+      </Pressable>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  mealItem: {
+    margin: 16,
+    borderRadius: 8,
+    overflow: "hidden",
+    backgroundColor: "white",
+    //Android shadow:
+    elevation: 4,
+    //iOs shadow (must have backgroundColor to work):
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    //to keep shadow on iOs and have android ripple effect with border radius:
+    overflow: Platform.OS === "android" ? "hidden" : "visible",
+  },
+  buttonPressed: {
+    opacity: 0.5,
+  },
+  innerContainer: {
+    //to keep border radius for iOs
+    borderRadius: 8,
+    overflow: "hidden",
+  },
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
+    margin: 8,
+  },
+  details: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 8,
+  },
+  detailItem: {
+    marginHorizontal: 4,
+    fontSize: 12,
+  },
+});
 
 export default MealItem;
