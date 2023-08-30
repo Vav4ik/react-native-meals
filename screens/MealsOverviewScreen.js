92fd12e524cb65e6015abe2eb43eb90f446fe927
Valdis Indricans <vav4ik@pop-os.localdomain>
2023-08-30 17:02:59 +0100

    End of lesson 102

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,15 +1,28 @@
+import { useLayoutEffect } from "react";
 import { FlatList, StyleSheet, Text, View } from "react-native";
 
-import { MEALS } from "../data/dummy-data";
+import { CATEGORIES, MEALS } from "../data/dummy-data";
 import MealItem from "../components/MealItem";
 
-const MealsOverviewScreen = ({ route }) => {
+const MealsOverviewScreen = ({ route, navigation }) => {
   //can use useRoute hook to get to route object, especially in nested objects
   const catId = route.params.categoryId;
 
   const displayedMeals = MEALS.filter(
     (meal) => meal.categoryIds.indexOf(catId) >= 0
   );
+
+  //this is almost the same as useEffect, but it runs simultaniously
+  //with component render (render waits a bit, I think) to make it visually nice, no jumps 
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === catId
+    ).title;
+
+    navigation.setOptions({
+      title: categoryTitle,
+    });
+  }, [catId, navigation]);
 
   const renderMealItem = (itemData) => {
     return <MealItem meal={itemData.item} />;
